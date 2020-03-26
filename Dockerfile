4d3207ec7b4f64bf12f29c873c4dc0e105a21aa8
Mark Tagab <proxyshadow@gmail.com>
2020-03-26 17:50:05 +0800

    adding output files

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,8 @@ RUN npm run build
 
 # Stage 2 - the production environment
 FROM nginx:alpine
-COPY nginx.conf /etc/nginx/conf.d/default.conf
 COPY --from=builder /app/build /usr/share/nginx/html
+COPY --from=builder /nginx.conf /etc/nginx/conf.d/default.conf
+
 EXPOSE 80
 CMD ["nginx", "-g", "daemon off;"]
---
+++ build/precache-manifest.f7f126dd1cf7f795fc52fd794afb5a8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03cb95341c1e42ee76d672946dba3dfb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd7d821dcab0420f5b96",
+    "url": "/static/css/main.28305139.chunk.css"
+  },
+  {
+    "revision": "d355f402a6df14fb06ce",
+    "url": "/static/js/2.2ca2ff4e.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.2ca2ff4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd7d821dcab0420f5b96",
+    "url": "/static/js/main.ef812c14.chunk.js"
+  },
+  {
+    "revision": "ef9a8d1a763064c87cc2",
+    "url": "/static/js/runtime-main.a468ba33.js"
+  }
+]);
