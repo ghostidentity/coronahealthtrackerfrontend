c4bbc4598d0d0f3e11c95fb61f24458635b0bc56
Mark Tagab <proxyshadow@gmail.com>
2020-04-07 20:45:10 +0800

    update tracking script

--- public/assets/js/analytics.js
+++ public/assets/js/analytics.js
@@ -1,7 +1,5 @@
 window.dataLayer = window.dataLayer || [];
-function gtag() {
-  dataLayer.push(arguments);
-}
-gtag("js", new Date());
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-gtag("config", "UA-46557668-10");
+gtag('config', 'G-N6ES1NE8L0');
